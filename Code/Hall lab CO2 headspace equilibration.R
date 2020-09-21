e029c95475bfdafa29e879cac2c43fdf3d0023a2
mtrentma <59029140+mtrentma@users.noreply.github.com>
2020-09-21 15:23:44 -0600

    Added carbonate chem for solving DIC

--- Code/Hall lab CO2 headspace equilibration.R
+++ Code/Hall lab CO2 headspace equilibration.R
@@ -59,8 +59,65 @@ Fw <- function(tempC, Cw.uatm){
   StmpCO2.umol
 }
 
+###########################################
+##Calculate DIC of water from pCO2 of water
+###########################################
 
+## Functions to determine equilibrium constants dependent on temperature of water when sampled
+K1calc<- function(temp_samp) { 10^( (-3404.71/(273.15+temp_samp)) + 14.844 -0.033*(temp_samp+273.15) )}
+K2calc<- function(temp_samp) { 10^( (-2902.39/(273.15+temp_samp)) + 6.498 -0.0238*(temp_samp+273.15) )}
 
+### Function to solve carbonate chemistry using CO2 & Alkalinity
+## See R Markdown file (XXXXXXX) for derivation of equations
+# H=H+ 
+# A=alkalinity in umol/L
+# C=CO2 in umol/L 
+# B=bicarbonate 
+# Ca=carbonate 
+# D=DIC in umol/L
+# A_check=alkalinity check
+Carbfrom_C_A <- function(K1, K2, C, A){
+  H <- (((-K1*C))-sqrt(((K1*C)^2)-(4*-1*A*2*K1*K2*C)))/(2*-1*A)
+  pH <- -1*log10(H)
+  B <- (K1*C)/H
+  Ca <- (K2*B)/H
+  D <- C + B + Ca
+  A_check <- B + 2*Ca
+  
+  l <- list(H_minus, pH, C, B, Ca, D, A, A_check)
+  names(l) <- c("H", "pH", "C", "B", "Ca", "D", "A","A check")
+  DIC<-D
+  return(l)
+}
+
+### Function to solve carbonate chemistry using DIC & Alkalinity
+## See R Markdown file (XXXXXXX) for derivation of equations
+# H=H+ 
+# A=alkalinity in umol/L
+# C=CO2 in umol/L 
+# B=bicarbonate 
+# Ca=carbonate 
+# D=DIC in umol/L
+# A_check=alkalinity check
+
+Carbfrom_D_A <- function(K1, K2, D, A){
+  B <- K1*(A-D)
+  C <- (A-(2*D))*K1*K2
+  H <- ((-1*B)+sqrt((B^2)-(4*A*C)))/(2*A)
+  
+  pH <- -1*log10(H)
+  
+  B <- (D1*K1*H)/((H^2)+(K1*H)+(K1*K2))
+  Ca <- (D1*K1*K2)/((H^2)+(K1*H)+(K1*K2))
+  
+  C <- (H_t*B_t)/K1
+  D2 <- C_t + B_t + Ca_t
+  A2_check <- B_t + 2*Ca_t
+  
+  l <- list(H_t, pH_t, C_t, B_t, Ca_t, D2, A, A2_check)
+  names(l) <- c("H", "pH", "C", "B", "Ca", "D", "A", "A check")
+  return(l)
+}
 
 
 ############
