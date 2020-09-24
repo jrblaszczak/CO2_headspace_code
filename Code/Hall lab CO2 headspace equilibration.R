df0b85bcc7c1bf0f1ffab658ab492e2bf675dda9
mtrentma <59029140+mtrentma@users.noreply.github.com>
2020-09-24 15:40:07 -0600

    Some troubleshooting and unit corrections. Test finished

--- Code/Hall lab CO2 headspace equilibration.R
+++ Code/Hall lab CO2 headspace equilibration.R
@@ -1,8 +1,8 @@
 
 ##Load data 
-data<-sum_file_final
+data<-read.csv("2020_09_23_CO2_QAQC.csv")
 
-##Load data
+##Format data
 #Constants
 R= 0.08205601 # gas constant in L*atm/mol*K
 dens=0.9998395 # density of freshwater
@@ -16,6 +16,18 @@ vol_samp=data$vol_samp          # volume of water sample in mL
 CO2_pre=data$CO2_pre           # pCO2 of headspace before equilibrium (zero if using zero air) # uatm or ppmv 
 CO2_post=data$CO2_post          # pCO2 of hs after equilibrium  # uatm or ppmv
 A=data$alkalinity                 # alkalinity in umol/L
+
+Calculate_CO2<-function(temp_equil, temp_samp, press_samp, vol_hs, vol_samp, CO2_pre,CO2_post, A){
+  StmpCO2 <-StmCO2fromSamp(temp_equil, temp_samp, press_samp, vol_hs, vol_samp, CO2_pre,CO2_post)
+  StmpCO2.umol <-Fw(tempC, StmpCO2)
+  K1<-K1calc(temp_equil)
+  K2<-K2calc(temp_equil)
+  Carbfrom_C_A(K1, K2, StmpCO2.umol , A)
+  delta_DIC<-DIC_correction(CO2_pre,StmpCO2,vol_hs,temp_equil,vol_samp)
+  DIC_corr<-Carb1$D+delta_DIC
+  CO2_corr<-Carbfrom_D_A(K1, K2, DIC_corr, A)
+}
+
 
 Calculate_CO2(temp_equil, temp_samp, press_samp, vol_hs, vol_samp, CO2_pre,CO2_post,A)
 
@@ -53,12 +65,12 @@ StmCO2fromSamp <- function(temp_equil, temp_samp, press_samp, vol_hs, vol_samp, 
   hsRatio <- vol_hs/vol_samp
   KH.equil <- KH.CO2(temp_equil) # mol L-1 atm-1
   KH.samp <- KH.CO2(temp_samp) # mol L-1 atm-1
-  StmCO2 <- (CO2_post*KH.equil+(hsRatio*(CO2_post-CO2_pre)/molV))/KH.samp
-  StmpCO2
-}
+  StmpCO2 <- (CO2_post*KH.equil+(hsRatio*(CO2_post-CO2_pre)/molV))/KH.samp
+  }
 
 ##### FUNCTION TO CONVERT pCO2 from uatm to umol/L #####  
 Fw <- function(tempC, StmpCO2){
+  temp_equil.K <- temp_equil + 273.15
   StmpCO2.umol <- StmpCO2*(1/0.08205601)*(1/(10^-3))*(1/temp_equil.K)/1000
   StmpCO2.umol
 }
@@ -80,28 +92,29 @@ K2calc<- function(temp_samp) { 10^( (-2902.39/(273.15+temp_samp)) + 6.498 -0.023
 # Ca=carbonate 
 # D=DIC in umol/L
 # A_check=alkalinity check
-Carbfrom_C_A <- function(K1, K2, StmpCO2.umol, A){
-  H <- (((-K1*StmpCO2.umol))-sqrt(((K1*StmpCO2.umol)^2)-(4*-1*A*2*K1*K2*StmpCO2.umol)))/(2*-1*A)
+Carbfrom_C_A <- function(K1, K2, StmpCO2.umol , A){
+  H <- (((-K1*StmpCO2.umol ))-sqrt(((K1*StmpCO2.umol )^2)-(4*-1*A*2*K1*K2*StmpCO2.umol )))/(2*-1*A)
   pH <- -1*log10(H)
-  B <- (K1*StmpCO2.umol)/H
+  B <- (K1*StmpCO2.umol )/H
   Ca <- (K2*B)/H
-  D <- StmpCO2.umol + B + Ca
+  D <- StmpCO2.umol  + B + Ca
   A_check <- B + 2*Ca
-  
-  l <- list(H_minus, pH, StmpCO2.umol, B, Ca, D, A, A_check)
-  names(l) <- c("H", "pH", "StmpCO2.umol", "B", "Ca", "D", "A","A check")
-  DIC<-D
-  return(l)
-}
+  Carb1 <- list(H, pH, StmpCO2.umol , B, Ca, D, A, A_check)
+  names(Carb1) <- c("H", "pH", "StmpCO2.umol ", "B", "Ca", "D", "A","A check")
+  }
 
 ### Function to calculate correction of perturbed sample for DIC before equilibration
 ## Originally from Dickenson et al. 2007--Chapter 4, highlighted again by Koschorreck et al. for freshwater
 
 DIC_correction<-function(CO2_pre,CO2_post,vol_hs,temp_equil,vol_samp){
-  delta_co2<-((CO2_pre-CO2_post)*vol_hs)/R*temp_equil
+  delta_co2<-(((CO2_pre/1000)-(CO2_post/1000))*(vol_hs/1000))/R*(temp_equil + 273.15)
   delta_DIC<-delta_co2/(vol_samp*dens)
   delta_DIC
   }
+
+### Apply correction to DIC data from Carb1####
+
+DIC_corr<-DIC$D+delta_DIC
 
 
 Carbfrom_D_A <- function(K1, K2, DIC_corr, A){
@@ -110,7 +123,7 @@ Carbfrom_D_A <- function(K1, K2, DIC_corr, A){
   B <- (DIC_corr*K1*H)/((H^2)+(K1*H)+(K1*K2))
   Ca <- (DIC_corr*K1*K2)/((H^2)+(K1*H)+(K1*K2))
   CO2 <- (H*B)/K1
-  D_check <- C + B + Ca
+  D_check <- CO2 + B + Ca
   A_check <- B + 2*Ca
   
   l <- list(H, pH, B, Ca, CO2, D_check, A_check)
